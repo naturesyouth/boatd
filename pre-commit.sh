005b55973aab0ce0a9ff434e36ddcc0631ef5e38
wih6 <wih6@aber.ac.uk>
2019-02-21 01:27:55 +0000

    Updates precommit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,7 +2,11 @@
 STASH_NAME="pre-commit-$(date +%s)"
 git stash save -q --keep-index $STASH_NAME
 
+tox
 
+RESULT=$?
+
+echo $RESULT
 
 STASHES=$(git stash list)
 if [[ $STASHES == "$STASH_NAME" ]]; then
